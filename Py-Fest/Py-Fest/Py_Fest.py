2d685731c720f6d4912cdcface0585aeb9146d2e
Justin <jjlon933@learner.dvusd.org>
2026-01-12 13:05:45 -0700

    Started Py_Fest
    Started assignment

--- Py-Fest/Py-Fest/Py_Fest.py
+++ Py-Fest/Py-Fest/Py_Fest.py
@@ -1,15 +1,22 @@
 # The initial lineup
+total_time = 0
+total_time += duration
 lineup = [
     ("Code Play", "Indie", 30),
     ("The Pythonistas", "Rock", 45),
     ("Syntax Error", "Metal", 60)
 ]
 
-# 1. Add the headliner
-headliner = ("The Byte Beats", "Synthwave", 90)
-lineup.append(headliner)
-print(lineup)
-remove_band = lineup.pop(0)
-lineup.append(remove_band)
-print(lineup)
-# Continue the logic below...
+
+while True:
+    print("\n---Py-Fest 2026 Stage Manager---")
+    print("1. View Lineup & Total Time")
+    print("2. Add a New Band")
+    print("3. Move First Band to End (Late Arrival)")
+    print("4. Remove Band by Name")
+    print("5. Move Band to SPecific Position")
+    print("6. Exit")
+    print(f"Total Festival Duration: {total_time} minutes")
+    choice = input("Select an option (1-6): ")
+    if choice == "1":
+        print("\n---Current Lineup---")
